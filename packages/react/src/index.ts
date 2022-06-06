ed0afbb11f8a72fdb8665a5ad8025e1f0fc56040
Brandon Clark <bclark@project44.com>
2022-06-06 11:40:06 -0500

    chore: adding export

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -4,6 +4,7 @@ export * from './components/Button';
 export * from './components/Card';
 export * from './components/Checkbox';
 export * from './components/CssBaseline';
+export * from './components/Dropdown';
 export * from './components/Flex';
 export * from './components/Icon';
 export * from './components/IconButton';
