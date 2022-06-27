110adbab610b01277e3b6c31cf886c17ae6f726b
Brandon Clark <yhbrandon@gmail.com>
2022-06-27 13:28:05 -0500

    ci: setting up release script

--- .config/beemo/jest.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  collectCoverageFrom: [
-    'packages/**/src/**/*.{ts,tsx}',
-    '!packages/**/src/**/*spec.{ts,tsx}',
-    '!packages/**/src/**/*stories.{ts,tsx}',
-  ],
-  setupFilesAfterEnv: ['@testing-library/jest-dom', 'jest-axe/extend-expect'],
-  testEnvironment: 'jsdom',
-};
