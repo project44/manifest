5b132bf823a5c6e32ff7f424c8e946b7b604cf1c
Brandon Clark <bclark@project44.com>
2022-06-28 10:05:23 -0500

    ci: adding codesandbox env for prs

---
+++ examples/create-react-app-typescript/src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Box, Flex, Link, Typography } from '@project44-manifest/react';
+
+function App() {
+  return (
+    <Box>
+      <Flex css={{ gap: '$small' }} orientation="vertical">
+        <Typography variant="body">
+          Edit <code>src/App.tsx</code> and save to reload.
+        </Typography>
+        <Link href="https://p44design.systems" target="_blank" rel="noopener noreferrer">
+          Learn Manifest
+        </Link>
+      </Flex>
+    </Box>
+  );
+}
+
+export default App;
