5758f6a1c1244653d9bf7d6fb1ceb2c7180d1ab7
Brandon Clark <bclark@project44.com>
2022-08-18 14:13:01 -0500

    feat: adding as prop to box

--- packages/react/src/components/Box/Box.tsx
+++ packages/react/src/components/Box/Box.tsx
@@ -7,11 +7,11 @@ import { useStyles } from './Box.styles';
 export type BoxProps = StyleProps;
 
 export const Box = createComponent<'div', BoxProps>((props, forwardedRef) => {
-  const { className: classNameProp, css, ...other } = props;
+  const { as: Comp = 'div', className: classNameProp, css, ...other } = props;
 
   const { className } = useStyles({ css });
 
   return (
-    <div {...other} className={cx(className, classNameProp, 'manifest-box')} ref={forwardedRef} />
+    <Comp {...other} className={cx(className, classNameProp, 'manifest-box')} ref={forwardedRef} />
   );
 });
