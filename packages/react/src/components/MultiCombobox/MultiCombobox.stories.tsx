4ea1162705f8a5d6e92456493a4cfb2472d93fa7
Brandon Clark <bclark@project44.com>
2022-09-23 16:47:37 -0500

    chore: removing commented out code

--- packages/react/src/components/MultiCombobox/MultiCombobox.stories.tsx
+++ packages/react/src/components/MultiCombobox/MultiCombobox.stories.tsx
@@ -12,104 +12,18 @@ export default {
   subcomponents: { SelectItem, SelectSection },
 } as ComponentMeta<typeof MultiCombobox>;
 
-const Template: ComponentStory<typeof MultiCombobox> = args => <MultiCombobox {...args} />;
+const Template: ComponentStory<typeof MultiCombobox> = args => (
+  <MultiCombobox {...args}>
+    <SelectItem key="late">Running Late</SelectItem>
+    <SelectItem key="early">Running Early</SelectItem>
+    <SelectItem key="estimated">Estimated Time</SelectItem>
+    <SelectItem key="planned">Planned Time</SelectItem>
+    <SelectItem key="actual">Actual Time</SelectItem>
+  </MultiCombobox>
+);
 
 export const Default = Template.bind({});
 
-Default.decorators = [
-  () => (
-    <MultiCombobox placeholder="Select time..." defaultSelectedKeys={['late']}>
-      <SelectItem key="late">Running Late</SelectItem>
-      <SelectItem key="early">Running Early</SelectItem>
-      <SelectItem key="estimated">Estimated Time</SelectItem>
-      <SelectItem key="planned">Planned Time</SelectItem>
-      <SelectItem key="actual">Actual Time</SelectItem>
-    </MultiCombobox>
-  ),
-];
-
-// export const Sizes = Template.bind({});
-
-// Sizes.decorators = [
-//   () => (
-//     <Flex css={{ gap: '$small' }} orientation="vertical">
-//       <MultiSelect size="medium">
-//         <SelectItem key="Ardvark">Ardvark</SelectItem>
-//         <SelectItem key="Kangaroo">Kangaroo</SelectItem>
-//         <SelectItem key="Snake">Snake</SelectItem>
-//       </MultiSelect>
-//       <MultiSelect size="small">
-//         <SelectItem key="Ardvark">Ardvark</SelectItem>
-//         <SelectItem key="Kangaroo">Kangaroo</SelectItem>
-//         <SelectItem key="Snake">Snake</SelectItem>
-//       </MultiSelect>
-//     </Flex>
-//   ),
-// ];
-
-// export const StartIcon = Template.bind({});
-
-// StartIcon.decorators = [
-//   () => (
-//     <MultiSelect startIcon={<Icon icon="search" />}>
-//       <SelectItem key="Ardvark">Ardvark</SelectItem>
-//       <SelectItem key="Kangaroo">Kangaroo</SelectItem>
-//       <SelectItem key="Snake">Snake</SelectItem>
-//     </MultiSelect>
-//   ),
-// ];
-
-// export const Label = Template.bind({});
-
-// Label.decorators = [
-//   () => (
-//     <MultiSelect label="Animal">
-//       <SelectItem key="Ardvark">Ardvark</SelectItem>
-//       <SelectItem key="Kangaroo">Kangaroo</SelectItem>
-//       <SelectItem key="Snake">Snake</SelectItem>
-//     </MultiSelect>
-//   ),
-// ];
-
-// export const HelperText = Template.bind({});
-
-// HelperText.decorators = [
-//   () => (
-//     <MultiSelect helperText="Please select an animal">
-//       <SelectItem key="Ardvark">Ardvark</SelectItem>
-//       <SelectItem key="Kangaroo">Kangaroo</SelectItem>
-//       <SelectItem key="Snake">Snake</SelectItem>
-//     </MultiSelect>
-//   ),
-// ];
-
-// export const Invalid = Template.bind({});
-
-// Invalid.decorators = [
-//   () => (
-//     <MultiSelect helperText="Error text" validationState="invalid">
-//       <SelectItem key="Ardvark">Ardvark</SelectItem>
-//       <SelectItem key="Kangaroo">Kangaroo</SelectItem>
-//       <SelectItem key="Snake">Snake</SelectItem>
-//     </MultiSelect>
-//   ),
-// ];
-
-// export const Controlled = Template.bind({});
-
-// Controlled.decorators = [
-//   () => {
-//     const [selected, setSelected] = React.useState(new Set(['Kangaroo']));
-
-//     return (
-//       <MultiSelect
-//         onSelectionChange={keys => setSelected(keys as Set<string>)}
-//         selectedKeys={selected}
-//       >
-//         <SelectItem key="Ardvark">Ardvark</SelectItem>
-//         <SelectItem key="Kangaroo">Kangaroo</SelectItem>
-//         <SelectItem key="Snake">Snake</SelectItem>
-//       </MultiSelect>
-//     );
-//   },
-// ];
+Default.args = {
+  placeholder: 'Select time...',
+};
