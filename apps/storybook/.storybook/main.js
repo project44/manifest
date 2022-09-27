fe628f46fc27e0d3558f54b4c1bffe2c8cd8f936
Brandon Clark <98107867+bclark-p44@users.noreply.github.com>
2022-09-27 15:41:07 -0500

    fix: moving storybook configs into an app (#106)
    * fix: moving storybook configs into an app
    
    * chore: adding changeset

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -2,5 +2,5 @@ module.exports = {
   addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
   core: { builder: 'webpack5' },
   staticDirs: ['public'],
-  stories: ['../src/components/**/*.stories.tsx'],
+  stories: ['../../../packages/react/src/**/*.stories.tsx'],
 };
