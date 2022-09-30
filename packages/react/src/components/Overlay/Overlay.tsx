eed0833d54c9d4eea0eacfd90bef1f99b95e0ff7
Brandon Clark <bclark@project44.com>
2022-09-30 12:04:40 -0500

    fix: fix build

--- packages/react/src/components/Overlay/Overlay.tsx
+++ packages/react/src/components/Overlay/Overlay.tsx
@@ -1,12 +1,7 @@
-import * as React from 'react';
 import { CSSTransition } from 'react-transition-group';
 import { Portal, PortalProps } from '../Portal';
 
 export interface OverlayProps extends PortalProps {
-	/**
-	 * The content to be rendered by the overlay
-	 */
-	children?: React.ReactNode;
 	/**
 	 * Whether the overlay is currently open.
 	 */
