f90bb66ac6f70ee3406a887c72f76a49155e71bb
Brandon Clark <bclark@project44.com>
2022-10-06 12:14:34 -0500

    feat: exporting a token object

---
+++ packages/design-tokens/src/tokens/color/border.ts
@@ -0,0 +1,15 @@
+import {
+	colorPaletteGreen600,
+	colorPaletteGrey100,
+	colorPaletteGrey200,
+	colorPaletteOrange600,
+	colorPaletteRed600,
+} from '../../constants';
+
+export const border = {
+	danger: colorPaletteRed600,
+	disabled: colorPaletteGrey100,
+	primary: colorPaletteGrey200,
+	success: colorPaletteGreen600,
+	warning: colorPaletteOrange600,
+};
---
+++ packages/design-tokens/src/tokens/color/brand.ts
@@ -0,0 +1,13 @@
+import {
+	colorBrandActive,
+	colorBrandDefault,
+	colorBrandGradient,
+	colorBrandHover,
+} from '../../constants';
+
+export const brand = {
+	active: colorBrandActive,
+	default: colorBrandDefault,
+	gradient: colorBrandGradient,
+	hover: colorBrandHover,
+};
