a3211a947bf7922d0c783b31f57d46c73afa7a57
Brandon Clark <bclark@project44.com>
2022-10-19 13:35:24 -0500

    feat: adding react-utils package

---
+++ packages/react-utilities/react-utils/tests/mergeRefs.test.ts
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { mergeRefs } from '../src';
+
+describe('@project44-manifest/react-utils - mergeRefs', () => {
+	it('should merge refs', () => {
+		const input = React.createElement('input', { type: 'text' });
+		const ref = React.createRef();
+		const callbackRef = jest.fn();
+
+		mergeRefs(ref, callbackRef)(input);
+
+		expect(ref.current).toBe(input);
+		expect(callbackRef).toHaveBeenCalledWith(input);
+	});
+
+	it('should ignore invalid refs', () => {
+		const input = React.createElement('input', { type: 'text' });
+		const ref = null;
+		const refObject = {};
+
+		mergeRefs(ref, refObject as React.RefObject<any>)(input);
+
+		// Just checking for an error
+		expect(true).toBeTruthy();
+	});
+});
