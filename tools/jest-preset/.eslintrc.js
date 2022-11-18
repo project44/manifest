26054e092f3005c0f8165d5c08b0ca88ba49219b
Brandon Clark <98107867+bclark-p44@users.noreply.github.com>
2022-11-18 15:37:46 -0600

    chore: improving github action workflow (#156)
    * chore: improving github action workflow
    
    * chore: adding doc deploy
    
    * chore: adding changeset
    
    * chore: add js to codeql check
    
    * chore: fix pr workflow
    
    * chore: add github token to pr title
    
    * chore: update the fetch depth for caching

--- tools/jest-preset/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	root: true,
-	extends: ['manifest'],
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json'],
-	},
-};
--- tools/jest-preset/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-	clean: true,
-	external: ['jest'],
-	format: ['cjs', 'esm'],
-	entry: ['src/index.ts'],
-	tsconfig: 'tsconfig.build.json',
-});
