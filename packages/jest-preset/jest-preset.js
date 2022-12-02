d4f9ccf234e6cab8c9524a2fe7330255fa2c6ecb
Brandon Clark <98107867+bclark-p44@users.noreply.github.com>
2022-12-02 11:57:23 -0600

    chore: moving tools to packages (#169)
    * chore: moving tools to packages
    
    * chore: adding changesets

---
+++ packages/jest-preset/jest-preset.js
@@ -0,0 +1,34 @@
+module.exports = {
+  collectCoverageFrom: ['**/{src,tests}/**/*.{js,jsx,ts,tsx}'],
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['coverage/', 'dist/', 'node_modules/', 'public/', '*.d.ts'].map(
+    (str) => str.replace(/\*+/g, '.*'),
+  ),
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
+  modulePathIgnorePatterns: ['node_modules/', 'coverage/', 'dist/'],
+  testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/{src,tests}/**/*.test.{js,jsx,ts,tsx}'],
+  testPathIgnorePatterns: ['dist/', 'node_modules/'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          transform: {
+            react: {
+              runtime: 'automatic',
+            },
+          },
+        },
+      },
+    ],
+  },
+};
