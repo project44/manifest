22e13a77050e33a110b8b82740f5711d8bceb582
Brandon Clark <bclark@project44.com>
2023-01-16 16:36:12 -0600

    chore: updating storybook theme

---
+++ .storybook/preview.tsx
@@ -0,0 +1,13 @@
+import { Provider } from '@project44-manifest/react';
+
+export const decorators = [
+  (Story) => (
+    <Provider>
+      <Story />
+    </Provider>
+  ),
+];
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
---
+++ .storybook/theme.ts
@@ -0,0 +1,11 @@
+import { create } from '@storybook/theming';
+import PackageJSON from '../packages/react/package.json';
+
+export default create({
+  base: 'light',
+  fontBase:
+    '"Inter", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif',
+  fontCode: '"Fira Mono", Courier, monospace',
+  brandTitle: `@projectt44-manfiest/react@${PackageJSON.version}`,
+  brandUrl: 'https://github.com/project44/manifest/tree/main/packages/react',
+});
