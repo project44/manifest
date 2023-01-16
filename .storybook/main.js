cfee346799f3d9d84f1f62cd145d90252f82f433
Brandon Clark <98107867+bclark-p44@users.noreply.github.com>
2023-01-16 16:10:57 -0600

    chore: updating stroybook to v7 (#186)
    * chore: updating storybook to v7
    
    * chore: adding changeset
    
    * chore: updating yarn.lock

--- .storybook/main.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	addons: ['@storybook/addon-a11y'],
-	core: { builder: 'webpack5' },
-	staticDirs: ['public'],
-	stories: ['../../../packages/**/stories/*.stories.@(js|jsx|ts|tsx|mdx)'],
-	typescript: {
-		reactDocgen: 'none',
-	},
-};
