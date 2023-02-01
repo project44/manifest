1013de8a8fe45e53b42bb12a6ca42de6e20d0e11
Brandon Clark <bclark@project44.com>
2023-02-01 14:36:17 -0600

    fix: downgrading to storybook stable

--- .storybook/main.ts
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports = {
-  stories: ['../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
-  framework: {
-    name: '@storybook/react-vite',
-  },
-};
---
+++ apps/storybook/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../../../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
+  },
+};
