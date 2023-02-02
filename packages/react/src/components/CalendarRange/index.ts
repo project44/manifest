7f92cf719642227b44c1173b07bbb2df3cf763c1
Brandon Clark <98107867+bclark-p44@users.noreply.github.com>
2023-02-02 12:42:05 -0600

    build: upgrading to moorepo 0.23 (#223)

--- packages/react/src/components/CalendarRange/index.ts
+++ packages/react/src/components/CalendarRange/index.ts
@@ -1,4 +1,2 @@
 export * from './CalendarRange';
-// Will be fixed when this is moved into its own package
-// eslint-disable-next-line import/no-unresolved
 export type { RangeValue } from '@react-types/shared';
