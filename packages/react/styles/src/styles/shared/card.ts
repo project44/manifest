a5c80dd546e8732907d00c3ca2e8dc4bb3488aca
Daniel <danielrobertson733@gmail.com>
2023-02-15 08:56:28 -0600

    refactor(Card): styles to shared style (#242)

---
+++ packages/react/styles/src/styles/shared/card.ts
@@ -0,0 +1,9 @@
+import { css } from '../../stitches';
+
+export const card = css({
+  backgroundColor: '$background-surface',
+  borderRadius: '$small',
+  boxShadow: '$medium',
+  boxSizing: 'border-box',
+  overflow: 'hidden',
+});
