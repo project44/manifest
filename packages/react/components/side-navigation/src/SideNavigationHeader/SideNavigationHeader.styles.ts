bdeadc2b04ec989662a2bab0db464d8829015edf
Brandon Clark <bclark@project44.com>
2023-02-15 17:42:54 -0600

    fix: fixing side nav header button position

--- packages/react/components/side-navigation/src/SideNavigationHeader/SideNavigationHeader.styles.ts
+++ packages/react/components/side-navigation/src/SideNavigationHeader/SideNavigationHeader.styles.ts
@@ -21,7 +21,7 @@ export const StyledSideNavigationHeaderContent = styled('div', {
 export const StyledSideNavigationHeaderButton = styled(IconButton, {
   position: 'absolute',
   right: pxToRem(8),
-  top: pxToRem(12),
+  top: pxToRem(8),
   transition: 'all 100ms cubic-bezier(0.4, 0.14, 0.3, 1) 0ms',
 });
 
