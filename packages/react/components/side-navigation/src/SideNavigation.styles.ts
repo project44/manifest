a7d3ff88ae1db60baddc047f4d3ce045a00b2ea3
Brandon Clark <bclark@project44.com>
2023-02-17 12:42:05 -0600

    chore: removing border

--- packages/react/components/side-navigation/src/SideNavigation.styles.ts
+++ packages/react/components/side-navigation/src/SideNavigation.styles.ts
@@ -3,7 +3,6 @@ import { pxToRem, styled } from '@project44-manifest/react-styles';
 export const StyledNavigation = styled('div', {
   boxSizing: 'border-box',
   backgroundColor: '$background-primary',
-  borderRight: '1px solid $colors$border-primary',
   display: 'flex',
   flexDirection: 'column',
   transition: 'width 200ms cubic-bezier(0.4, 0.14, 0.3, 1) 0ms',
