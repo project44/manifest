8f954ec8f52223efb9731f591f29e46ead2f8d39
Daniel Robertson <danielrobertson733@gmail.com>
2023-03-10 12:18:51 -0600

    feat(website): menu docs

---
+++ apps/website/src/pages/demos/menu.tsx
@@ -0,0 +1,17 @@
+import { Menu, MenuGroup, MenuItem } from '@project44-manifest/react';
+import { Carrier, Route } from '@project44-manifest/react-icons';
+import { withDemoWrapper } from '../../components/DemoWrapper';
+
+function MenuExample() {
+  return (
+    <Menu css={{ maxWidth: 240 }}>
+      <MenuItem label="Track" startIcon={<Route />} />
+      <MenuGroup label="Initiate" startIcon={<Carrier />}>
+        <MenuItem label="Shipments" />
+        <MenuItem label="Templates" />
+      </MenuGroup>
+    </Menu>
+  );
+}
+
+export default withDemoWrapper(MenuExample);
