7b415a4ab7fd60d948b81d7f7d87bd690c4c471f
Brandon Clark <98107867+bclark-p44@users.noreply.github.com>
2023-03-10 12:15:45 -0800

    feat: adding layout docs (#303)

---
+++ apps/website/src/pages/demos/flex-wrap.tsx
@@ -0,0 +1,30 @@
+import { Box, Flex } from '@project44-manifest/react';
+import { withDemoWrapper } from '../../components/DemoWrapper';
+
+function FlexExample() {
+  return (
+    <Flex wrap gap="small">
+      <Box css={{ backgroundColor: '$palette-green-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-purple-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-pink-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-blue-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-red-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-orange-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-grey-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-green-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-purple-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-pink-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-green-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-purple-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-pink-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-green-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-purple-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-pink-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-green-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-purple-500', size: 64 }} />
+      <Box css={{ backgroundColor: '$palette-pink-500', size: 64 }} />
+    </Flex>
+  );
+}
+
+export default withDemoWrapper(FlexExample);
