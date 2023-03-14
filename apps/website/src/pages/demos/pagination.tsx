ba1cdbbb1eebee606289964bc59fdc39d2d9df59
Daniel <danielrobertson733@gmail.com>
2023-03-14 14:58:09 -0500

    docs: portal docs (#308)
    * docs(website): dropdown demo
    
    * docs(website): pagination demo
    
    * docs(website): portal docs
    
    * docs(website): fix pagination format ci failure
    
    ---------
    
    Co-authored-by: Brandon Clark <98107867+bclark-p44@users.noreply.github.com>

---
+++ apps/website/src/pages/demos/pagination.tsx
@@ -0,0 +1,8 @@
+import { Pagination } from '@project44-manifest/react';
+import { withDemoWrapper } from '../../components/DemoWrapper';
+
+function PaginationExample() {
+  return <Pagination totalRowCount={100} />;
+}
+
+export default withDemoWrapper(PaginationExample);
