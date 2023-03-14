f691296e41ce1c9b6b7bcc9b73636184288d7897
Daniel Robertson <danielrobertson733@gmail.com>
2023-03-14 11:33:51 -0500

    docs(website): fix pagination format ci failure

--- apps/website/src/pages/demos/pagination.tsx
+++ apps/website/src/pages/demos/pagination.tsx
@@ -2,9 +2,7 @@ import { Pagination } from '@project44-manifest/react';
 import { withDemoWrapper } from '../../components/DemoWrapper';
 
 function PaginationExample() {
-  return (
-    <Pagination totalRowCount={100} />
-  );
+  return <Pagination totalRowCount={100} />;
 }
 
 export default withDemoWrapper(PaginationExample);
