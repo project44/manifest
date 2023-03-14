f5465290d91ff668cab44266c723289164cd5bd0
Daniel <danielrobertson733@gmail.com>
2023-03-14 14:57:22 -0500

    docs(website): spinner (#310)

---
+++ apps/website/src/pages/demos/spinner.tsx
@@ -0,0 +1,8 @@
+import { Spinner } from '@project44-manifest/react';
+import { withDemoWrapper } from '../../components/DemoWrapper';
+
+function SpinnerExample() {
+  return <Spinner />;
+}
+
+export default withDemoWrapper(SpinnerExample);
