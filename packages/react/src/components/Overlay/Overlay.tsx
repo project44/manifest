9715096d468ada0b59d97167f901c71710fe2488
Brandon Clark <98107867+bclark-p44@users.noreply.github.com>
2023-03-23 09:33:24 -0700

    Revert "fix: wrapping overlay elements in provider for correct text rendering (#330)" (#333)
    This reverts commit 61daff26f77ba68100af41766b235af05e898304.

--- packages/react/src/components/Overlay/Overlay.tsx
+++ packages/react/src/components/Overlay/Overlay.tsx
@@ -1,17 +1,14 @@
 import { CSSTransition } from 'react-transition-group';
 import { Portal } from '../Portal';
-import { Provider } from '../Provider';
 import type { OverlayProps } from './Overlay.types';
 
 export function Overlay(props: OverlayProps) {
   const { children, containerRef, isOpen } = props;
 
   const contents = (
-    <Provider>
-      <CSSTransition unmountOnExit classNames="manifest-overlay" in={isOpen} timeout={200}>
-        {children}
-      </CSSTransition>
-    </Provider>
+    <CSSTransition unmountOnExit classNames="manifest-overlay" in={isOpen} timeout={200}>
+      {children}
+    </CSSTransition>
   );
 
   return <Portal containerRef={containerRef}>{contents}</Portal>;
