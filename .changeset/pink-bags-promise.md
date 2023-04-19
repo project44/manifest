800be727f518cc76d4307f657540aa2846202cd9
Daniel Robertson <danielrobertson733@gmail.com>
2023-04-19 13:15:30 -0700

    feat(hooks): add useChildrenTextContent

---
+++ .changeset/pink-bags-promise.md
@@ -0,0 +1,5 @@
+---
+'@project44-manifest/react': minor
+---
+
+add useChildrenTextContent hook
---
+++ packages/react/src/hooks/useChildrenTextContent.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export function useChildrenTextContent(children?: React.ReactNode[]) {
+  const childrenTextContent = React.useMemo(
+    () =>
+      children === undefined
+        ? ''
+        : React.Children.toArray(children)
+            .filter((child) => typeof child === 'string')
+            .join(' '),
+    [children],
+  );
+
+  return childrenTextContent;
+}
