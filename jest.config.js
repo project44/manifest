7fbeb6554db432cf6dcac17ecf125059c06ad1a9
Brandon Clark <bclark@project44.com>
2023-05-22 14:43:14 -0500

    chore: migrate to turbo from moonrepo

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-
-const setupFilesAfterEnv: string[] = [];
-const setupFilePath = path.join(process.env.MOON_WORKSPACE_ROOT ?? process.cwd(), 'tests/setup.ts');
-
-// Only include the file if it exists, otherwise Jest throws an error
-if (fs.existsSync(setupFilePath)) {
-  setupFilesAfterEnv.push(setupFilePath);
-}
-
-const config = {
+module.exports = {
   collectCoverage: false,
-  collectCoverageFrom: ['**/{src,tests}/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['packages/**/{src,tests}/**/*.{js,jsx,ts,tsx}'],
   coverageDirectory: './coverage',
   coveragePathIgnorePatterns: [
     'node_modules/',
@@ -31,11 +20,7 @@ const config = {
     },
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
-  moduleNameMapper: {
-    '\\.(css|sass|scss|less|gif|png|jpg|jpeg|svg|gql|graphql|yml|yaml)$':
-      require.resolve('./fileMock.js'),
-  },
-  setupFilesAfterEnv,
+  setupFilesAfterEnv: ['./tests/setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
   testMatch: ['**/tests/**/*.test.{js,jsx,ts,tsx}'],
   transform: {
@@ -53,5 +38,3 @@ const config = {
     ],
   },
 };
-
-export default config;
