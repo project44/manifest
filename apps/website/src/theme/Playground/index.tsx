c81dbac61f33573817de6e1be2a4442bb05c7f3d
Daniel Robertson <danielrobertson733@gmail.com>
2023-07-18 16:33:51 +0100

    feat(website): live typescript code blocks

--- apps/website/src/theme/Playground/index.tsx
+++ apps/website/src/theme/Playground/index.tsx
@@ -1,20 +1,17 @@
-import { LiveEditor, LiveError, LivePreview, LiveProvider, LiveProviderProps } from 'react-live';
+import { LiveEditor, LiveError, LivePreview, LiveProvider } from 'react-live';
 import { usePrismTheme } from '@docusaurus/theme-common';
 import { BrowserWindow } from '@site/src/components';
 
-export default function Playground({
-  children,
-  transformCode,
-  ...props
-}: Omit<LiveProviderProps, 'children' | 'ref'> & { children: string }) {
+export default function Playground({ children, ...props }: { children: string }) {
   const prismTheme = usePrismTheme();
 
   return (
-    <div className="playgroud">
+    <div className="playground">
       <LiveProvider
         code={children.trim()}
         theme={prismTheme}
-        transformCode={transformCode ?? ((code) => code.trim())}
+        // eslint-disable-next-line react/jsx-no-bind
+        transformCode={(code) => code.trim()}
         {...props}
       >
         <BrowserWindow>
