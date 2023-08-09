19595a6662301d16b2de4f74a574e7f8ab1854d2
Michał Maj <mmaj@project44.com>
2023-08-09 13:16:55 +0200

    fix: fix local navigation styling, add white mode variant

--- packages/react/src/components/LocalNavigation/LocalNavigation.tsx
+++ packages/react/src/components/LocalNavigation/LocalNavigation.tsx
@@ -1,17 +1,55 @@
 import * as React from 'react';
 import { CSS, cx } from '@project44-manifest/react-styles';
 import type { ForwardRefComponent } from '@project44-manifest/react-types';
+import { createContext } from '../../utils/context';
 import { StyledNavigation } from './LocalNavigation.styles';
+
+export type LocalNavigationVariant = 'primary' | 'secondary';
+
+/* -------------------------------------------------------------------------------------------------
+ * LocalNavigationContext
+ * -----------------------------------------------------------------------------------------------*/
+
+interface LocalNavigationContext {
+  variant?: LocalNavigationVariant;
+}
+
+export const [LocalNavigationProvider, useLocalNavigation] = createContext<LocalNavigationContext>({
+  name: 'LocalNavigationContext',
+});
+
+/* -------------------------------------------------------------------------------------------------
+ * LocalNavigation
+ * -----------------------------------------------------------------------------------------------*/
 
 export interface LocalNavigationProps {
   /** Theme aware style object */
   css?: CSS;
+  /**
+   * The display variant.
+   *
+   * @default 'primary'
+   */
+  variant?: LocalNavigationVariant;
 }
 
 export const LocalNavigation = React.forwardRef((props, forwardedRef) => {
-  const { as, className: classNameProp, css, ...other } = props;
+  const { as, children, className: classNameProp, css, variant = 'primary', ...other } = props;
+
+  const context = React.useMemo(() => ({ variant }), [variant]);
 
   const className = cx('manifest-local-navigation', classNameProp);
 
-  return <StyledNavigation {...other} ref={forwardedRef} as={as} className={className} css={css} />;
+  return (
+    <StyledNavigation
+      {...other}
+      ref={forwardedRef}
+      as={as}
+      className={className}
+      css={css}
+      variant={variant}
+    >
+      <LocalNavigationProvider value={context}>{children}</LocalNavigationProvider>
+    </StyledNavigation>
+  );
 }) as ForwardRefComponent<'div', LocalNavigationProps>;
--- packages/react/stories/LocalNavigation.stories.tsx
+++ packages/react/stories/LocalNavigation.stories.tsx
@@ -1,4 +1,4 @@
-import { LocalNavigation, LocalNavigationItem } from '../src';
+import { LocalNavigation, LocalNavigationItem, Stack, Typography } from '../src';
 
 export default {
   title: 'Components/LocalNavigation',
@@ -13,6 +13,25 @@ export const Default = () => (
     <LocalNavigationItem>Carriers</LocalNavigationItem>
     <LocalNavigationItem>Containers</LocalNavigationItem>
   </LocalNavigation>
+);
+
+export const Variant = () => (
+  <Stack gap="large">
+    <Typography variant="subtextBold">Primary</Typography>
+    <LocalNavigation>
+      <LocalNavigationItem isSelected>Overview</LocalNavigationItem>
+      <LocalNavigationItem>Lanes</LocalNavigationItem>
+      <LocalNavigationItem>Carriers</LocalNavigationItem>
+      <LocalNavigationItem>Containers</LocalNavigationItem>
+    </LocalNavigation>
+    <Typography variant="subtextBold">Secondary</Typography>
+    <LocalNavigation variant="secondary">
+      <LocalNavigationItem isSelected>Overview</LocalNavigationItem>
+      <LocalNavigationItem>Lanes</LocalNavigationItem>
+      <LocalNavigationItem>Carriers</LocalNavigationItem>
+      <LocalNavigationItem>Containers</LocalNavigationItem>
+    </LocalNavigation>
+  </Stack>
 );
 
 export const Selected = () => (
