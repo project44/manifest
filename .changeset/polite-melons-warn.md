643bfe6eabc84fc84fad53bc6f66133f79e9de9a
michalmaj-p44 <90175251+michalmaj-p44@users.noreply.github.com>
2023-08-09 20:59:24 +0200

    fix: fix local navigation styling, add white mode variant (#449)

---
+++ .changeset/polite-melons-warn.md
@@ -0,0 +1,6 @@
+---
+'@project44-manifest/react': patch
+---
+
+Fix LocalNavigation default styling to previous blue one. Add new LocalNavigation variant for white
+mode.
--- packages/react/src/components/LocalNavigation/LocalNavigation.styles.ts
+++ packages/react/src/components/LocalNavigation/LocalNavigation.styles.ts
@@ -2,10 +2,19 @@ import { styled } from '@project44-manifest/react-styles';
 
 export const StyledNavigation = styled('div', {
   alignItems: 'center',
-  bgColor: '$background-primary',
-  borderBottom: '1px solid $border-primary',
   display: 'flex',
   gap: '$small',
   minH: '56px',
   px: '$large',
+  variants: {
+    variant: {
+      primary: {
+        bgColor: '$background-top-nav',
+      },
+      secondary: {
+        bgColor: '$background-primary',
+        borderBottom: '1px solid $border-primary',
+      },
+    },
+  },
 });
