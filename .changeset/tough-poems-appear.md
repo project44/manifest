9d40057d65eff76b26227845fa64d9dc7b92b8ae
Dan Morton <88048149+dmorton-p44@users.noreply.github.com>
2023-08-10 14:26:19 -0500

    chore(Skeleton): Removed use of react.useId in skeleton component (#452)
    * chore(Skeleton): Removed use of react.useId in skeleton component
    
    * INITIATE-2157: Changelog
    
    * INITIATE-2157: Fixed code styles

---
+++ .changeset/tough-poems-appear.md
@@ -0,0 +1,5 @@
+---
+'@project44-manifest/react': minor
+---
+
+Removed use of useId from manifest skeleton component
--- packages/react/src/components/Skeleton/Skeleton.tsx
+++ packages/react/src/components/Skeleton/Skeleton.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { cx } from '@project44-manifest/react-styles';
 import { StyledSkeleton } from './Skeleton.styles';
 import type { SkeletonProps } from './Skeleton.types';
@@ -6,17 +5,10 @@ import type { SkeletonProps } from './Skeleton.types';
 export function Skeleton(props: SkeletonProps) {
   const { children, className: classNameProp, css, ...other } = props;
 
-  const id = React.useId();
-
   const className = cx('manifest-skeleton', classNameProp);
 
   return (
-    <StyledSkeleton
-      {...other}
-      className={className}
-      css={css}
-      uniqueKey={id} // needed for SSR: https://github.com/danilowoz/react-content-loader#sedrver-side-rendering-ssr---match-snapshot
-    >
+    <StyledSkeleton {...other} className={className} css={css}>
       {children}
     </StyledSkeleton>
   );
