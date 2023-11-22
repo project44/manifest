e3745c92fdeedb8d627af86a70f1bad299a64079
Vivek jain <vivek.jain@project44.com>
2023-11-22 18:55:35 +0530

    fix(update storybook): updated storybook to v7

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,8 +1,24 @@
-module.exports = {
+import { dirname, join } from 'path';
+const config = {
   stories: ['../../../packages/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
-  framework: '@storybook/react',
-  core: {
-    builder: 'webpack5',
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-a11y'),
+  ],
+
+  framework: {
+    name: getAbsolutePath('@storybook/react-webpack5'),
+    options: {},
+  },
+
+  docs: {
+    autodocs: true,
   },
 };
+
+export default config;
+
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
