471ba6a1484121d402df604bf30881a85355eaad
Ajit-p44 <151926527+Ajit-p44@users.noreply.github.com>
2024-08-28 23:41:44 +0530

    feat(Pill): add new variant

---
+++ .changeset/lemon-plants-fix.md
@@ -0,0 +1,5 @@
+---
+'@project44-manifest/react': major
+---
+
+Add a new pill variant
--- packages/react/src/components/Pill/Pill.tsx
+++ packages/react/src/components/Pill/Pill.tsx
@@ -17,7 +17,7 @@ export interface PillOptions<T extends As = PillElement> extends Options<T>, Sty
    *
    * @default 'indigo'
    */
-  colorScheme?: 'indigo' | 'red';
+  colorScheme?: 'indigo' | 'red' | 'yellow';
   /**
    * The icon to render in the pill.
    */
